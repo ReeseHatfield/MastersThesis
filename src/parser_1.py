6710411492b60d440d8904ac964a3d76908238d8
ReeseHatfield <hatfield.69@wright.edu>
2024-05-19 11:07:53 -0400

    add intial col. 'heatmap'

--- src/parser_1.py
+++ src/parser_1.py
@@ -1,26 +1,26 @@
 import dask.dataframe as dd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Load the data using dask
+
 df = dd.read_csv('data/consumption/energy_consumption_by_country.csv')
 
-# Extract the numeric columns
+
 numeric_df = df[['Consumption MMBtu', 'Expenditure US Dollars']]
 
-# Compute the mean values for a quicker overview
-mean_values = numeric_df.mean().compute()
 
-# Convert to a numpy array
-data_array = numeric_df.compute().values
+summary_df = numeric_df.groupby(df['State Name']).mean().compute()
 
-# Plot the heatmap
+
+data_array = summary_df.values
+
+
 plt.figure(figsize=(10, 8))
 plt.imshow(data_array, cmap='viridis', aspect='auto')
 plt.colorbar(label='Values')
-plt.title('Heatmap of Energy Consumption and Expenditure')
+plt.title('Heatmap of Summarized Energy Consumption and Expenditure by State')
 plt.xlabel('Features')
-plt.ylabel('Data Points')
-plt.xticks(ticks=np.arange(len(mean_values)), labels=mean_values.index, rotation=90)
-plt.yticks(ticks=np.arange(data_array.shape[0]), labels=df['County Name'].compute())
+plt.ylabel('States')
+plt.xticks(ticks=np.arange(data_array.shape[1]), labels=['Consumption MMBtu', 'Expenditure US Dollars'], rotation=0)
+plt.yticks(ticks=np.arange(data_array.shape[0]), labels=summary_df.index)
 plt.show()
