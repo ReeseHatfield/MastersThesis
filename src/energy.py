161c43aea95c44f1c599d955d3dba34c746a776d
ReeseHatfield <hatfield.69@wright.edu>
2024-06-25 12:57:17 -0400

    finally fix county data avg.

--- src/energy.py
+++ src/energy.py
@@ -1,19 +1,42 @@
+# Important notes:
+# - Counties do not occur at the same density across the US?
+# - I might begin to look into normalizing the density, aswell
+# - Really dense in the south
+# - Less dense in the west coast
+# - very sparse in Alaska
+# - After normalizing instead of dropping, much more topographic looking data
+
 import pandas as pd
 import vtk
 import numpy as np
-
+data = pd.read_csv('data/cleaned/energy_usage.csv')
 data = pd.read_csv('data/cleaned/energy_usage.csv')
 
-# drop duplicate county, will make this an average later
-filtered_data = data.drop_duplicates(subset=['County'])
+# Group by 'State' and 'County' and calculate mean of 'Consumption MMBtu'
+consumption_mean = data.groupby(['State', 'County'])['Consumption MMBtu'].mean().reset_index()
 
+# Merge the mean consumption back with the original data to keep the latitude and longitude
+merged_data = pd.merge(data, consumption_mean, on=['State', 'County'], suffixes=('', '_mean'))
+
+# Drop duplicates to keep unique rows for visualization (in case of multiple lat/long for same state and county)
+unique_data = merged_data.drop_duplicates(subset=['State', 'County'])
+
+# Select relevant columns
+filtered_data = unique_data[['State', 'County', 'Latitude', 'Longitude', 'Consumption MMBtu_mean']]
+filtered_data.rename(columns={'Consumption MMBtu_mean': 'Consumption MMBtu'}, inplace=True)
 
 latitudes = filtered_data['Latitude'].to_list()
 longitudes = filtered_data['Longitude'].to_list()
 consumptions = filtered_data['Consumption MMBtu'].to_list()
 
+# 10 is good for visualization
+# 1 might be better for compatability
+scale_factor = 10 
+
 # scale down points, this was the problem
-consumptions_scaled = np.array(consumptions) / np.max(consumptions) * 10  
+consumptions_scaled = np.array(consumptions) / np.max(consumptions) * scale_factor
+for val in consumptions_scaled:
+    print(val)
 
 
 points = vtk.vtkPoints()
@@ -35,7 +58,7 @@ mapper.SetInputConnection(vertex_filter.GetOutputPort())
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
-actor.GetProperty().SetPointSize(10)  # can change point size as needed
+actor.GetProperty().SetPointSize(1)  
 
 
 renderer = vtk.vtkRenderer()
